0980f0a7b95354ea80bf6992930d5a8b58aa8e73
Abiodun Gbenga <gbengaemma22@gmail.com>
2024-05-17 17:20:52 +0100

    "dependncy formatting "

--- lib/helpers/dependencies.dart
+++ lib/helpers/dependencies.dart
@@ -1,6 +1,9 @@
 import 'package:get/get.dart';
 import 'package:habit_app/controller/onboarding_controller.dart';
 
-Future<void> init()async{
-  Get.lazyPut(() => OnboardingController(), fenix: true,);
+Future<void> init() async {
+  Get.lazyPut(
+    () => OnboardingController(),
+    fenix: true,
+  );
 }
