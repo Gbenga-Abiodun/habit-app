31ab48b426c0ef79c6995e6a1cf5098b945d81cf
Abiodun Gbenga <gbengaemma22@gmail.com>
2024-06-18 23:33:42 +0100

    top space edit

--- lib/widgets/top_space.dart
+++ lib/widgets/top_space.dart
@@ -3,11 +3,11 @@ import 'package:flutter/material.dart';
 import '../utils/dimensions.dart';
 
 class TopSpace extends StatelessWidget {
-  const TopSpace({Key? key}) : super(key: key);
+  const TopSpace({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return  SizedBox(
+    return SizedBox(
       height: Dimensions.height12 * 3.75,
     );
   }
